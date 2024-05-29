a0466799e8ddeb6141d7ea9ac6c8168222528a71
Jonsy86 <jonasnolte86@gmail.com>
2024-05-29 12:44:52 +0200

    + Bird

---
+++ com/cc/java/Bird.java
@@ -0,0 +1,20 @@
+package com.cc.java;
+
+public class Bird extends HoneyBee {
+
+    @Override
+    public String doYourJob() {
+        // TODO Auto-generated method stub
+        return "But i am a biiird!";
+    }
+
+    @Override
+    public String fly() {
+        // TODO Auto-generated method stub
+        return "I can fly at 20mph!";
+    }
+
+
+
+
+}
